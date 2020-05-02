8c3e51d1a8ce533251b5682dac77f0a1669b391f
kage// <koiru3822fs@gmail.com>
2020-05-03 01:44:12 +0900

    base codes for OpenGL

--- Basic_CG/simple_window.cpp
+++ Basic_CG/simple_window.cpp
@@ -1,12 +1,13 @@
+#define FREEGLUT_STATIC
 #include <GL/glut.h> // ライブラリ用ヘッダファイルの読み込み
 
 // 表示部分をこの関数で記入
 void display(void) {
-	glClearColor(1.0, 1.0, 1.0, 1.0);  // 消去色指定
+    glClearColor(1.0, 1.0, 1.0, 1.0);  // 消去色指定
 	glClear(GL_COLOR_BUFFER_BIT);     // 画面消去
 
 	/* ここに描画に関するプログラムコードを入れる */
-
+	
 	glFlush(); // 画面出力
 }
 
