eb479b6d14b00b7ab29f05d4b79a7b671fea3551
kage// <koiru3822fs@gmail.com>
2020-05-26 00:06:13 +0900

    Add 1

--- Basic_CG/simple_window.cpp
+++ Basic_CG/simple_window.cpp
@@ -1,22 +1,93 @@
 #define FREEGLUT_STATIC
-#include <GL/glut.h> // ライブラリ用ヘッダファイルの読み込み
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <GL/glut.h>
 
-// 表示部分をこの関数で記入
-void display(void) {
-    glClearColor(1.0, 1.0, 1.0, 1.0);  // 消去色指定
-	glClear(GL_COLOR_BUFFER_BIT);     // 画面消去
+using namespace std;
 
-	/* ここに描画に関するプログラムコードを入れる */
+// 3次元ベクトルを扱うためのクラス
+class Vector3d {
+public:
+	double x, y, z;
+	Vector3d() { x = y = z = 0; }
+	Vector3d(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+	void set(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+
+	// 長さを1に正規化する
+	void normalize() {
+		double len = length();
+		x /= len; y /= len; z /= len;
+	}
+
+	// 長さを返す
+	double length() { return sqrt(x * x + y * y + z * z); }
+
+	// s倍する
+	void scale(const double s) { x *= s; y *= s; z *= s; }
+
+	// 加算の定義
+	Vector3d operator+(Vector3d v) { return Vector3d(x + v.x, y + v.y, z + v.z); }
+
+	// 減算の定義
+	Vector3d operator-(Vector3d v) { return Vector3d(x - v.x, y - v.y, z - v.z); }
+
+	// 内積の定義
+	double operator*(Vector3d v) { return x * v.x + y* v.y + z * v.z; }
+
+	// 外積の定義
+	Vector3d operator%(Vector3d v) { return Vector3d(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
 	
-	glFlush(); // 画面出力
-}
+	// 代入演算の定義
+	Vector3d& operator=(const Vector3d& v){ x = v.x; y = v.y; z = v.z; return (*this); }
 
-// メインプログラム
-int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);          // ライブラリの初期化
-	glutInitWindowSize(400, 400);  // ウィンドウサイズを指定
-	glutCreateWindow(argv[0]);      // ウィンドウを作成
-	glutDisplayFunc(display);       // 表示関数を指定
-	glutMainLoop();                 // イベント待ち
+	// 加算代入の定義
+	Vector3d& operator+=(const Vector3d& v) { x += v.x; y += v.y; z += v.z; return (*this); }
+
+	// 減算代入の定義
+	// ★加算代入の定義を参考に、減算代入のコードを記述してみる
+
+	// 値を出力する
+	void print() { printf("Vector3d(%f %f %f)\n", x, y, z); }
+};
+
+// マイナスの符号の付いたベクトルを扱えるようにするための定義 例：b=(-a); のように記述できる
+Vector3d operator-( const Vector3d& v ) { return( Vector3d( -v.x, -v.y, -v.z ) ); }
+
+// ベクトルと実数の積を扱えるようにするための定義 例： c=5*a+2*b; c=b*3; のように記述できる
+Vector3d operator*( const double& k, const Vector3d& v ) { return( Vector3d( k*v.x, k*v.y, k*v.z ) );}
+Vector3d operator*( const Vector3d& v, const double& k ) { return( Vector3d( v.x*k, v.y*k, v.z*k ) );}
+
+// ベクトルを実数で割る操作を扱えるようにするための定義 例： c=a/2.3; のように記述できる
+Vector3d operator/( const Vector3d& v, const double& k ) { return( Vector3d( v.x/k, v.y/k, v.z/k ) );}
+
+
+int main(int argc, char**argv) {
+	// ベクトルの加算の例
+	Vector3d a(2, 3, 4);
+	Vector3d b(3, 5, -2);
+	Vector3d c(2, -1, 1);
+	Vector3d ans_a = a - b;
+	Vector3d ans_b = 3 * a - 2 * b;
+	double ans_c = a * b;
+	Vector3d ans_d = a % b;
+	ans_d.normalize();
+	Vector3d ans_e = (a + 2 * b) % c;
+
+
+	// output
+	printf("(a): ");
+	ans_a.print();
+	printf("(b): ");
+	ans_b.print();
+	printf("(c): %f\n", ans_c);
+	printf("(d): ");
+	ans_d.print();
+	printf("(e): ");
+	ans_e.print();
+
+	// Visual Studio でコンソールがすぐに閉じないようにするためには次のコメントアウトをはずす
+	system("pause"); 
 	return 0;
 }
+
